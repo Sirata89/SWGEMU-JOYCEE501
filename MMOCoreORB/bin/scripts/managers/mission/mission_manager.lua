2b3f54a0aee5123d88f2b2a9422f070d81829eb4
sirata89 <nicholas.kent.t@gmail.com>
2026-05-23 20:06:47 +0000

    Double destroy mission reward scaling factors

--- MMOCoreORB/bin/scripts/managers/mission/mission_manager.lua
+++ MMOCoreORB/bin/scripts/managers/mission/mission_manager.lua
@@ -116,6 +116,6 @@ destroyMissionDifficultyRandomDistance = 0
 --    <BaseReward> + <DifficultyRewardFactor> * <difficultyLevel> + 
 --    rand(<RandomReward>) + rand(<DifficutlyRandomReward * <difficultyLevel>)
 destroyMissionBaseReward = 0
-destroyMissionDifficultyRewardFactor = 375
+destroyMissionDifficultyRewardFactor = 750
 destroyMissionRandomReward = 0
-destroyMissionDifficultyRandomReward = 15
+destroyMissionDifficultyRandomReward = 30
